d38b4d4731bcfc7cbdf2281e4b26e8b241c54eb7
chaoyang.zhang <chaoyang.zhang@ttpod.com>
2015-03-22 14:24:39 +0800

    Initial Commit

---
+++ GNGeneralNullValue/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GNGeneralNullValue
+//
+//  Created by DjangoZhang on 15/3/22.
+//  Copyright (c) 2015年 DjangoZhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
